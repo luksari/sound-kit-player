4a9ea20f94764db59fc23475d6f0ef85bcc04970
Lukasz Tyszkiewicz <luksari23@gmail.com>
2017-04-20 22:47:27 +0200

    Adding mobile sound playing function

--- js/script.js
+++ js/script.js
@@ -8,15 +8,31 @@ function playSoundKey(e){
   keyEl.style.transitionDuration = `${audioEl.duration * 0.5}s`;
   keyEl.classList.add('playing');
 }
-  function playSoundTouch(e){
+
+function playSoundTouch(e){
     let keyEl = e.target;
-    if(keyEl.nodeName == "SPAN" || keyEl.nodeName == "KBD") keyEl = keyEl.parentElement;
+    if(keyEl.nodeName == "SPAN" || keyEl.nodeName == "KBD")
+      keyEl = keyEl.parentElement;
 
     const audioEl = document.querySelector(`audio[data-key="${keyEl.getAttribute("data-key")}"]`);
-    if (!audioEl)
-        return;
+
+    const soundSprite = [
+      {start: 0, end: 3000},
+      {start: 3500, end: 6789}
+      ];
+      function playSoundFile(idx) {
+          audioEl.currentPosition = soundSprite[idx].start;
+          var x = setInterval(function() {
+            if(audioEl.currentPosition >= soundSprite[idx].end) {
+              audioEl.pause();
+              clearInterval(x);
+            }
+          }, 50);
+        }
+    if (!audioEl) return;
     audioEl.currentTime = 0;
     audioEl.play();
+    playSoundFile(0);
     keyEl.style.transitionDuration = `${audioEl.duration * 0.5}s`;
     keyEl.classList.add('playing');
 
