d4c112c665cc1f67aa6190f86d0c7c37dbf9ffc1
Lukasz Tyszkiewicz <luksari23@gmail.com>
2017-04-20 23:02:02 +0200

    Style, Script changes
    Prefixed all of styles and delted mobile version support

--- js/script.js
+++ js/script.js
@@ -1,51 +1,58 @@
 
-function playSoundKey(e){
+function playSoundKey(e) {
   const audioEl = document.querySelector(`audio[data-key="${e.keyCode}"]`);
   const keyEl = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-  if (!audioEl) return;
+  if (!audioEl)
+    return;
   audioEl.currentTime = 0;
   audioEl.play();
   keyEl.style.transitionDuration = `${audioEl.duration * 0.5}s`;
   keyEl.classList.add('playing');
 }
+// function playSoundTouch(e) {
+//   let keyEl = e.target;
+//   if (keyEl.nodeName == "SPAN" || keyEl.nodeName == "KBD")
+//     keyEl = keyEl.parentElement;
+//
+//   const audioEl = document.querySelector(`audio[data-key="${keyEl.getAttribute("data-key")}"]`);
+//
+//   const soundSprite = [
+//     {
+//       start: 0,
+//       end: 3000
+//     }, {
+//       start: 3500,
+//       end: 6789
+//     }
+//   ];
+//   function playSoundFile(idx) {
+//     audioEl.currentPosition = soundSprite[idx].start;
+//     var x = setInterval(function() {
+//       if (audioEl.currentPosition >= soundSprite[idx].end) {
+//         audioEl.pause();
+//         clearInterval(x);
+//       }
+//     }, 50);
+//   }
+//
+//   if (!audioEl)
+//     return;
+//   audioEl.currentTime = 0;
+//   audioEl.play();
+//   playSoundFile(0);
+//   keyEl.style.transitionDuration = `${audioEl.duration * 0.5}s`;
+//   keyEl.classList.add('playing');
+//
+// }
 
-function playSoundTouch(e){
-    let keyEl = e.target;
-    if(keyEl.nodeName == "SPAN" || keyEl.nodeName == "KBD")
-      keyEl = keyEl.parentElement;
+window.addEventListener('keydown', playSoundKey, false);
 
-    const audioEl = document.querySelector(`audio[data-key="${keyEl.getAttribute("data-key")}"]`);
+function endTransition(e) {
+  this.classList.remove('started');
+}
 
-    const soundSprite = [
-      {start: 0, end: 3000},
-      {start: 3500, end: 6789}
-      ];
-      function playSoundFile(idx) {
-          audioEl.currentPosition = soundSprite[idx].start;
-          var x = setInterval(function() {
-            if(audioEl.currentPosition >= soundSprite[idx].end) {
-              audioEl.pause();
-              clearInterval(x);
-            }
-          }, 50);
-        }
-    if (!audioEl) return;
-    audioEl.currentTime = 0;
-    audioEl.play();
-    playSoundFile(0);
-    keyEl.style.transitionDuration = `${audioEl.duration * 0.5}s`;
-    keyEl.classList.add('playing');
-
-  }
-
-  window.addEventListener('keydown', playSoundKey, false);
-
-  function removeTransition(e) {
-      this.classList.remove('playing');
-  }
-
-  const keys = document.querySelectorAll('.key');
-  keys.forEach(keyEl => {
-      keyEl.addEventListener('touchstart', playSoundTouch, false);
-      keyEl.addEventListener('transitionend', removeTransition, false);
-  });
+const keys = document.querySelectorAll('.key');
+keys.forEach(keyEl => {
+  // keyEl.addEventListener('touchstart', playSoundTouch, false);
+  keyEl.addEventListener('transitionend', endTransition, false);
+});
