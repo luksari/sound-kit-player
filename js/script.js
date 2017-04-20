2aeac0b95e190c3feea497eb0bd8afa03d3cf720
Lukasz Tyszkiewicz <luksari23@gmail.com>
2017-04-20 13:02:48 +0200

    mobile version touch event

--- js/script.js
+++ js/script.js
@@ -1,21 +1,35 @@
-window.addEventListener('keydown', function(e) {
-    const audioEl = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    const keyEl = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+
+function playSoundKey(e){
+  const audioEl = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const keyEl = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+  if (!audioEl) return;
+  audioEl.currentTime = 0;
+  audioEl.play();
+  keyEl.style.transitionDuration = `${audioEl.duration * 0.5}s`;
+  keyEl.classList.add('playing');
+}
+  function playSoundTouch(e){
+    let keyEl = e.target;
+    if(keyEl.nodeName == "SPAN" || keyEl.nodeName == "KBD") keyEl = keyEl.parentElement;
+
+    const audioEl = document.querySelector(`audio[data-key="${keyEl.getAttribute("data-key")}"]`);
     if (!audioEl)
         return;
     audioEl.currentTime = 0;
     audioEl.play();
     keyEl.style.transitionDuration = `${audioEl.duration * 0.5}s`;
     keyEl.classList.add('playing');
-});
 
-function removeTransition(e) {
+  }
 
-    this.classList.remove('playing');
+  window.addEventListener('keydown', playSoundKey, false);
 
-}
+  function removeTransition(e) {
+      this.classList.remove('playing');
+  }
 
-const keys = document.querySelectorAll('.key');
-keys.forEach(keyEl => {
-    keyEl.addEventListener('transitionend', removeTransition);
-});
+  const keys = document.querySelectorAll('.key');
+  keys.forEach(keyEl => {
+      keyEl.addEventListener('touchstart', playSoundTouch, false);
+      keyEl.addEventListener('transitionend', removeTransition, false);
+  });
