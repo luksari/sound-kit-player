7ac46c3ebdee3e9f97cdfdcc53d0afcf4fe6d4f1
Lukasz Tyszkiewicz <luksari23@gmail.com>
2017-04-22 16:51:19 +0200

    Js and style changes

--- js/script.js
+++ js/script.js
@@ -9,43 +9,8 @@ function playSoundKey(e) {
   keyEl.style.transitionDuration = `${audioEl.duration * 0.5}s`;
   keyEl.classList.add('started');
 }
-// function playSoundTouch(e) {
-//   let keyEl = e.target;
-//   if (keyEl.nodeName == "SPAN" || keyEl.nodeName == "KBD")
-//     keyEl = keyEl.parentElement;
-//
-//   const audioEl = document.querySelector(`audio[data-key="${keyEl.getAttribute("data-key")}"]`);
-//
-//   const soundSprite = [
-//     {
-//       start: 0,
-//       end: 3000
-//     }, {
-//       start: 3500,
-//       end: 6789
-//     }
-//   ];
-//   function playSoundFile(idx) {
-//     audioEl.currentPosition = soundSprite[idx].start;
-//     var x = setInterval(function() {
-//       if (audioEl.currentPosition >= soundSprite[idx].end) {
-//         audioEl.pause();
-//         clearInterval(x);
-//       }
-//     }, 50);
-//   }
-//
-//   if (!audioEl)
-//     return;
-//   audioEl.currentTime = 0;
-//   audioEl.play();
-//   playSoundFile(0);
-//   keyEl.style.transitionDuration = `${audioEl.duration * 0.5}s`;
-//   keyEl.classList.add('playing');
-//
-// }
 
-window.addEventListener('keydown', playSoundKey, false);
+window.addEventListener('keydown', playSoundKey);
 
 function endTransition(e) {
   this.classList.remove('started');
@@ -53,6 +18,5 @@ function endTransition(e) {
 
 const keys = document.querySelectorAll('.key');
 keys.forEach(keyEl => {
-  // keyEl.addEventListener('touchstart', playSoundTouch, false);
-  keyEl.addEventListener('transitionend', endTransition, false);
+  keyEl.addEventListener('transitionend', endTransition);
 });
