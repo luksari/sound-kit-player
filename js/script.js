7a34aed0d06ec492d34af3c612b3e182d1ae76ce
Lukasz Tyszkiewicz <luksari23@gmail.com>
2017-04-27 16:34:17 +0200

    Adding security
    Adding security

--- js/script.js
+++ js/script.js
@@ -1,22 +1,24 @@
+(function (){
+  'use strict';
+  function playSoundKey(e) {
+    const audioEl = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const keyEl = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    if (!audioEl)
+      return;
+    audioEl.currentTime = 0;
+    audioEl.play();
+    keyEl.style.transitionDuration = `${audioEl.duration * 0.5}s`;
+    keyEl.classList.add('started');
+  }
 
-function playSoundKey(e) {
-  const audioEl = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-  const keyEl = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-  if (!audioEl)
-    return;
-  audioEl.currentTime = 0;
-  audioEl.play();
-  keyEl.style.transitionDuration = `${audioEl.duration * 0.5}s`;
-  keyEl.classList.add('started');
-}
+  window.addEventListener('keydown', playSoundKey);
 
-window.addEventListener('keydown', playSoundKey);
+  function endTransition(e) {
+    this.classList.remove('started');
+  }
 
-function endTransition(e) {
-  this.classList.remove('started');
-}
-
-const keys = document.querySelectorAll('.key');
-keys.forEach(keyEl => {
-  keyEl.addEventListener('transitionend', endTransition);
-});
+  const keys = document.querySelectorAll('.key');
+  keys.forEach(keyEl => {
+    keyEl.addEventListener('transitionend', endTransition);
+  });
+})();
