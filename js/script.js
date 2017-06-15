c4059af233c319bb2abccbdb710c35ae91e17242
Lukasz Tyszkiewicz <luksari23@gmail.com>
2017-06-15 11:13:13 +0200

    volume slider
    Added volume slider and minor design changes

--- js/script.js
+++ js/script.js
@@ -3,9 +3,11 @@
   function playSoundKey(e) {
     const audioEl = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     const keyEl = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    const volumeEl = document.querySelector("input#volume-change");
     if (!audioEl)
       return;
     audioEl.currentTime = 0;
+    audioEl.volume = volumeEl.value;
     audioEl.play();
     keyEl.style.transitionDuration = `${audioEl.duration * 0.5}s`;
     keyEl.classList.add('started');
